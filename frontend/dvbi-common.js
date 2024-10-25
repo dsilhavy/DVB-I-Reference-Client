e08c371869d16cadde63327d7b626e5224394677
Jordi J. Gimenez <87380947+jordijoangimenez@users.noreply.github.com>
2024-10-25 14:42:49 +0200

    Add files via upload

--- frontend/dvbi-common.js
+++ frontend/dvbi-common.js
@@ -1,5 +1,5 @@
 //var PROVIDER_LIST = INSTALL_LOCATION + "/backend/servicelist_registry.php";
-var PROVIDER_LIST = "http://192.168.178.20:3333/dvb-i/5g-mag/5gmag-service-list-offering.xml";
+var PROVIDER_LIST = "http://192.168.0.101:3333/dvb-i/5g-mag/5gmag-service-list-offering.xml";
 
 /** LCN_services_only
  * set to true to only include services in the selected region that are included in the reevant LCN table.
